d0bb3e761a6aa45efe93fdc44f5849899b4a8f27
yabibal <yabibalhunie@gmail.com>
2024-07-11 19:33:03 +0300

    response correction

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,8 +2,7 @@ import "./Footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
 
 function Footer() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -13,9 +12,9 @@ function Footer() {
   };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
   const currentYear = new Date().getFullYear();
@@ -27,49 +26,85 @@ function Footer() {
           <InstagramIcon />
           <YouTubeIcon />
         </div>
-        <div className="footer_data">
-          <div>
-            <div>
-              <ul>
-                <li>Audio Description</li>
-                <li>Investor Relations</li>
-                <li>Legal Notice</li>
-                {!isMobile && <li className="service_code">Service Code</li>}
-              </ul>
+        <div>
+          {!isMobile ? (
+            <div className="footer_data">
+              <div>
+                <ul>
+                  <li>Audio Description</li>
+                  <li>Investor Relations</li>
+                  <li>Legal Notice</li>
+                  <li className="service_code">Service Code </li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>Help Center</li>
+                  <li>Jobs</li>
+                  <li>Cookie Preferences</li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>Gift Cards</li>
+                  <li>Terms of Use</li>
+                  <li>Corporate Information</li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>Media Center</li>
+                  <li>Privacy</li>
+                  <li>Contact Us</li>
+                </ul>
+              </div>
             </div>
-            <div>
-              <ul>
-                <li>Help Center</li>
-                <li>Jobs</li>
-                <li>Cookie Preferences</li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <div>
-              <ul>
-                <li>Gift Cards</li>
-                <li>Terms of Use</li>
-                <li>Corporate Information</li>
-              </ul>
-            </div>
-            <div>
-              <ul>
-                <li>Media Center</li>
-                <li>Privacy</li>
-                <li>Contact Us</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="footer_copyright">
-          {isMobile && (
-            <div className="service_code">
-              <p>Service Code</p>
-            </div>
+          ) : (
+            <section className="footer_data">
+              <div>
+                <div>
+                  <ul>
+                    <li>Audio Description</li>
+                    <li>Investor Relations</li>
+                    <li>Legal Notice</li>
+                    <li>Service Code</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li>Help Center</li>
+                    <li>Jobs</li>
+                    <li>Cookie Preferences</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <ul>
+                    <li>Gift Cards</li>
+                    <li>Terms of Use</li>
+                    <li>Corporate Information</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li>Media Center</li>
+                    <li>Privacy</li>
+                    <li>Contact Us</li>
+                  </ul>
+                </div>
+              </div>
+            </section>
           )}
-          <div className="copy_write">
-            <p>© 1997-{currentYear} Netflix, Inc.</p>
+          <div className="footer_copyright">
+            {isMobile && (
+              <div className="service_code">
+                <p>Service Code</p>
+              </div>
+            )}
+            <div className="copy_write">
+              <p>© 1997-{currentYear} Netflix, Inc.</p>
+            </div>
           </div>
         </div>
       </div>
